ff112c819f2c14375bf4a65b1f004a280a93601b
Kanthasuwamy-Kumaran <rkkumaran834@gmail.com>
2023-12-07 15:58:46 +0530

    reupdate

--- index.js
+++ index.js
@@ -12,15 +12,29 @@
 // })
 
 
+
+/*Load Environment Variables:
+This line imports and configures the 'dotenv' module, which is commonly used to 
+load environment variables from a .env file into the process.env object.*/
 require('dotenv').config();
 
+/*Import Dependencies
+Here, the code imports the Express.js 
+framework and the Mongoose library for MongoDB.*/
 const express = require('express');
 const mongoose = require('mongoose');
-const mongoString = process.env.DATABASE_URL;
 
+/*Database Connection:
+It establishes a connection to the MongoDB database 
+using the connection string from the environment variable DATABASE_URL.*/
+const mongoString = process.env.DATABASE_URL;
 mongoose.connect(mongoString);
 const database = mongoose.connection;
 
+/*Database Event Handling:
+These lines handle events related to the database connection. 
+If there's an error, it's logged. 
+Once the connection is successful, it logs a message.*/
 database.on('error', (error) => {
     console.log(error)
 })
@@ -30,12 +44,16 @@ database.once('connected', () => {
     console.log('Database Connected');
 })
 
+/*Create Express App
+An Express app is created, and
+it's configured to parse JSON in the request body using express.json().*/
 const app = express();
-
 app.use(express.json());
 
 
-//server 
+/*Start Server:
+It starts the Express server on port 3000 and logs 
+a message when the server is successfully started.*/
 app.listen(3000, () => {
     console.log(`Server Started at ${3000}`)
 })
@@ -47,7 +65,16 @@ app.listen(3000, () => {
 // })
 
 
+
+/*Import Routes
+It imports the route definitions from the './routes/routes' file. 
+Routes are typically used to define the API endpoints and their corresponding handlers.*/
 const routes = require('./routes/routes');
+//this employee path for routes
 // const routes = require('./routes/employeeRoutes');
 
+/*Use Route Middleware
+Use Routes:
+It tells the Express app to use the routes defined in the 'routes' module 
+when requests with the '/api' prefix are received.*/
 app.use('/api', routes)
--- model/model.js
+++ model/model.js
@@ -1,6 +1,14 @@
 //Schema
+
+/*Import Mongoose:
+The code imports the Mongoose library to define a schema 
+and create a model for working with MongoDB.*/
 const mongoose = require('mongoose');
 
+/*Define Data Schema:
+This block of code defines a Mongoose schema for a 'Data' model. 
+The schema has two fields: 'name' and 'age'. Both fields are required (required: true). 
+The 'name' field is of type String, and the 'age' field is of type Number.*/
 const dataSchema = new mongoose.Schema({
     name: {
         required: true,
@@ -12,4 +20,8 @@ const dataSchema = new mongoose.Schema({
     }
 })
 
+/*Export Model:
+This line exports the Mongoose model. The mongoose.model function takes two arguments: 
+the name of the model ('Data' in this case) and the schema (defined earlier). 
+This allows you to interact with MongoDB using the defined schema.*/
 module.exports = mongoose.model('Data', dataSchema)
